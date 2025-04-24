b1e51e0dcb0e6e5da64fdcad860928a5877ea96c
sebstianobecchetti <sebastiano.becchetti@studenti.unipg.it>
2025-04-24 13:42:01 +0200

    FIXED DOTFILE

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -7,8 +7,8 @@ return {
     views = {
       cmdline = {
         position = {
-          row = -1,      -- l'ultima riga (in basso)
-          col = 0,       -- inizia da sinistra
+          row = -1, -- l'ultima riga (in basso)
+          col = 0, -- inizia da sinistra
         },
         size = {
           width = "100%", -- larga quanto la finestra
@@ -17,4 +17,3 @@ return {
     },
   },
 }
-
